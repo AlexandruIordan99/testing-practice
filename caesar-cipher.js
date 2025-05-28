116c0b4a8a5f29019e0f1988a9fd80e3c39eb5ca
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-28 17:51:37 +0200

    Updated cipher to loop from end to start of the array

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -5,25 +5,41 @@ function caesarCipher(stringToEncrypt, factor){
   //add that to the encryptedString variable
   //return encrypted string
   let alphabetArrayUppercase =[];
+  let alphabetArrayLowercase =[];
   let i;
   for(let i=65; i <=90; i++){
     alphabetArrayUppercase.push(String.fromCharCode(i));
   }
-  console.log(alphabetArrayUppercase);
-  let alphabetArrayLowercase =[];
   for (i = 97; i <=122; i++){
     alphabetArrayLowercase.push(String.fromCharCode(i));
   }
-  console.log(alphabetArrayLowercase);
   let stringToEncryptArray = stringToEncrypt.split("");
 
   for(let j = 0; j < stringToEncryptArray.length; j++){
-    if(j in alphabetArrayLowercase || j in alphabetArrayUppercase){
-
+    if(stringToEncryptArray[j] in alphabetArrayLowercase){
+      if(j+factor > alphabetArrayLowercase.length){
+        stringToEncryptArray[j] = alphabetArrayLowercase[factor - (alphabetArrayLowercase.length-1-j)];
+      } else {
+        stringToEncryptArray[j] = alphabetArrayLowercase[j + factor];
+      }
     }
+    else if (stringToEncryptArray[j] in alphabetArrayLowercase){
+      if(j+factor > alphabetArrayLowercase.length){
+        stringToEncryptArray[j] = alphabetArrayLowercase[factor - (alphabetArrayLowercase.length-1-j)];
+      } else{
+        stringToEncryptArray[j] = alphabetArrayUppercase[j + factor];
+      }
+    }
+  }
+
+
+  for (let i = 0; i < stringToEncryptArray.length; i++){
+
   }
 
   return "ended"
 }
 
-caesarCipher("A", 1);
+console.log(caesarCipher("A", 1));
+
+module.exports=caesarCipher;
