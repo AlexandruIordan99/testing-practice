195bd0a5912809f16040ac6a3745c265a2702e40
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-28 19:07:29 +0200

    Added factor value to test calls

--- caesar-cipher.test.js
+++ caesar-cipher.test.js
@@ -1,12 +1,12 @@
 const caesarCipher = require ("./caesar-cipher.js");
 
 it("Caesar cipher by 4", () => {
-  expect(caesarCipher("Where are my apples?"))
+  expect(caesarCipher("Where are my apples?", 4))
     .toBe("Alivi evi qc ettpiw?");
 });
 
 
 it("Caesar cipher by 7", () => {
-  expect(caesarCipher("Where are my apples?"))
+  expect(caesarCipher("Where are my apples?", 7))
       .toBe("Dolyl hyl tf hwwslz?");
 });
